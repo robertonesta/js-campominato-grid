2d0408b7c3499f8421ae918b97f50ac9453fc4bf
RobertoNesta <robertonesta1993@gmail.com>
2023-03-17 16:12:50 +0100

    adding the grid by JS

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+// Creo una variabile dove salvo il container delle 64 celle (container-grid)
+
+const container = document.querySelector(".container");
+
+// creo un max numero di celle
+
+let nMax = 100;
+
+// creo un ciclo n volte per le celle (dove n in questo caso sta per 64)
+
+for (let i = 0; i < nMax; i++) {
+  const cell = `<div class="cell"></div>`;
+  container.innerHTML += cell;
+}
+
+// seleziono una cella che ha classe cell e active
+
+const cellEl = document.querySelectorAll(".cell")
+
+// aggiungo eventlistner per la classe active ciclando per la lunghezza dell'array cellEl
+
+
+for (let i = 0; i < cellEl.length; i++) {
+    const thisCell = cellEl[i];
+    console.log(thisCell)
+    thisCell.addEventListener("click", function() {
+        thisCell.classList.toggle("bg_selected")
+        // this.classList.add("active")
+        console.log("Changed the color")
+    })
+}
