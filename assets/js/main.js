41cc01893d5515cc62984f9b6a6fa19645829ea9
RobertoNesta <robertonesta1993@gmail.com>
2023-03-17 21:43:24 +0100

    fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,31 @@
 // Creo una variabile dove salvo il container delle 64 celle (container-grid)
-const container = document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
-// creo un max numero di celle
+const playEl = document.querySelector(".play")
 
-let nMax = 100;
+playEl.addEventListener('click', function(){
 
-// creo un ciclo n volte per le celle (dove n in questo caso sta per 64)
+  containerEl.innerHTML = "";
 
-for (let i = 0; i < nMax; i++) {
-  const cell = `<div class="cell">${i}</div>`;
-  container.innerHTML += cell;
-}
+  const difficultyEl = document.getElementById("difficulty")
+  console.log(difficultyEl)
+  //creare una funzione in cui l'nMax varia a seconda della difficoltà
 
-// seleziono una cella che ha classe cell e active
+  let nMax = difficultyEl.value;
 
-const cellEl = document.querySelectorAll(".cell")
+  // creo un ciclo n volte per le celle (dove n in questo caso sta per 100 o 81 o 49)
+  for (let i = 1; i <= nMax; i++) {
+  const cell = `<div class="cell" style="width: calc(100% / ${Math.sqrt(nMax)}"><p class="m-0">${i}</p></div>`;
+  containerEl.innerHTML += cell;
+  }
+  // seleziono una cella che ha classe cell e active
 
-// aggiungo eventlisner per la classe active ciclando per la lunghezza dell'array cellEl
+  const cellEl = document.querySelectorAll(".cell")
+
+  // aggiungo eventlistener per la classe active ciclando per la lunghezza dell'array cellEl
 
 
-for (let i = 0; i < cellEl.length; i++) {
+  for (let i = 0; i < cellEl.length; i++) {
     const thisCell = cellEl[i];
     console.log(thisCell)
     thisCell.addEventListener("click", function() {
@@ -27,7 +33,9 @@ for (let i = 0; i < cellEl.length; i++) {
         // this.classList.add("active")
         console.log("Changed the color")
     })
-
-
-    
 }
+})
+
+
+
+
